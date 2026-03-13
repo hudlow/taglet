8438e33c7168728279beef042678b7263492a86b
Dan Hudlow <dan@hudlow.org>
2026-03-12 21:52:01 -0500

    initial docs, tests, and implementation

---
+++ readme.md
@@ -0,0 +1,194 @@
+# taglet
+
+taglet is a zero-dependency utility for generating code. It _mostly_ exists to
+improve the ergonomics of native ECMAScript [tagged templates][ecmascript]. It
+can also be used to generate _partially_ abstract code which is useful when the
+same code has multiple target formats (such as TypeScript and JavaScript).
+
+[ecmascript]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
+
+## Usage
+
+taglet exports a single tagged template function called `taglet()` which returns
+has special accommodations for multi-line templates and returns
+a `taglet.Taglet` — an object whose `toString()` function renders the template,
+but which also exposes the ability to transform the arguments before rendering.
+
+### Inline templates
+
+Inline templates provide lazy evaluation and transformation, but do not modify whitespace:
+
+```javascript
+const line = taglet`items[${1}] = ${2};`;
+
+// render as-is:
+console.log(line.toString()); // items[1] = 2;
+
+// transform numbers:
+const newLine = line.transform((arg) => (typeof arg === "number" ? arg * 2 : arg));
+console.log(newLine.toString()); // items[2] = 4;
+
+// supply substitute arguments at render-time:
+console.log(line.toString(8, '"hippo"')); // items[8] = "hippo";
+```
+
+With some narrow exceptions, all characters are preserved literally, including
+backslashes. This means you don't have to double-backslash in most
+circumstances:
+
+```javascript
+const line = taglet`const regexp = /\//;`;
+console.log(line.toString()); // const regexp = /\//;
+```
+
+### Block templates
+
+Block templates are formatted very similarly to [block scalars in
+YAML][yaml-block-scalars]. They begin with a header line consisting of:
+
+1. A _style_ indicator: `|` for "literal" or `>` for "folded."
+2. An optional _indention_ indicator: the number of spaces of non-semantic
+   indention for the content. If omitted, the indention amount is inferred to be
+   the leading whitespace of the first non-blank line.
+3. An optional _chomping_ indicator: `-` to strip trailing empty lines and `+`
+   to keep trailing empty lines. If omitted, a single trailing empty line is
+   preserved or appended.
+
+Like YAML, when both optional indicators are present, they may be in either
+order. Unlike YAML, any whitespace in the header line is illegal.
+
+```javascript
+// Literal block template with inferred indention and default chomping
+const block = taglet`|
+  line one
+  line two
+`;
+
+console.log(block.toString()); // "line one\nline two\n"
+```
+
+#### Folding
+
+Like YAML, for two adjacent lines to be folded, neither can have any semantic
+leading whitespace. Unlike YAML, both lines must be non-empty.
+
+Folding behavior may be controlled directly with backslash-escaped line endings:
+
+- For literal block templates, this folds a line with the subsequent line,
+  _without inserting a space_.
+- For folded block templates, this ensures a line will not be folded.
+
+Backslash-escaping is illegal on empty lines.
+
+```javascript
+// Folded block template
+const block = taglet`>
+  part one
+  part two\
+  part three
+`;
+
+console.log(block.toString()); // "part one part two\npart three\n"
+```
+
+```javascript
+// Literal block template with escaped line ending
+const block = taglet`|
+  line one\
+  -thousand
+`;
+
+console.log(block.toString()); // "line one-thousand\n"
+```
+
+#### Indention
+
+Like YAML:
+
+- When an indention indicator is present, the non-semantic indention prefix is
+  the number of _spaces_ indicated.
+- Blank lines that consist of the indention prefix or any truncation of the
+  indention prefix are treated equivalently: as empty lines.
+- Any other line which does not begin with the indention prefix is illegal.
+- Non-empty blank lines preceding the first non-blank line are illegal.
+
+Unlike YAML, when indention is inferred, the prefix may consist of tabs or
+even a mixture of spaces and tabs.
+
+```javascript
+// Semantic indention in block template
+const block = taglet`|
+  line one
+    line two
+`;
+
+console.log(block.toString()); // "line one\n  line two\n"
+```
+
+```javascript
+// Indention indicator for block template
+const block = taglet`|0
+  line one
+    line two
+`;
+
+console.log(block.toString()); // "  line one\n    line two\n"
+```
+
+### Chomping
+
+Like YAML:
+
+- The strip indicator (`-`) truncates all trailing empty lines
+- The keep indicator (`+`) preserves all trailing empty lines
+- The default behavior is to preserve or, if necessary, append a single trailing
+  empty line.
+
+Also like YAML, all non-empty lines are treated the same even if they are blank.
+
+```javascript
+const block1 = taglet`|
+  line one
+  line two
+
+
+`;
+
+const block2 = taglet`|
+  line one
+  line two`;
+
+console.log(block1.toString() === block2.toString()); // true
+```
+
+```javascript
+const block = taglet`|+
+  line one
+  line two
+
+
+`;
+
+console.log(block.toString()); // "line one\nline two\n\n\n"
+```
+
+```javascript
+const block = taglet`|-
+  line one
+  line two
+
+
+`;
+
+console.log(block.toString()); // "line one\nline two"
+```
+
+[yaml-block-scalars]: https://yaml.org/spec/1.2.2/#81-block-scalar-styles
+
+### Glossary
+
+- **blank line**: A line containing only whitespace
+- **empty line**: A line containing no semantic characters
+- **semantic** Preserved in a processed template
+- **folding** Concatenating two lines together, separated by a space
+- **chomping** Truncating trailing empty lines
---
+++ taglet.test.ts
@@ -0,0 +1,509 @@
+/*
+ * © Copyright 2026 Dan Hudlow
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the “Software”), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { describe, test } from "node:test";
+import { strict as assert } from "node:assert";
+import { taglet } from "./taglet.js";
+
+describe("taglet", () => {
+  test("basic substitution", () => {
+    assert.equal(taglet`1 ${2} 3`.toString(), "1 2 3");
+  });
+  test("basic transformation", () => {
+    assert.equal(
+      taglet`1 ${2} 3`.transform((x) => (typeof x === "number" ? x * 2 : x)).toString(),
+      "1 4 3",
+    );
+  });
+  test("preserves whitespace", () => {
+    assert.equal(
+      taglet`1
+      2
+      3
+      4`.toString(),
+      `1
+      2
+      3
+      4`,
+    );
+  });
+  describe("block templates", () => {
+    describe("block header", () => {
+      test("chomping indicator can precede indention indicator", () => {
+        assert.equal(
+          taglet`|-12
+            1
+          `.toString(),
+          "1",
+        );
+      });
+      test("indention indicator can precede chomping indicator", () => {
+        assert.equal(
+          taglet`|12-
+            1
+          `.toString(),
+          "1",
+        );
+      });
+      test("whitespace before header is treated as inline", () => {
+        assert.equal(
+          taglet` |
+            1
+          `.toString(),
+          " |\n            1\n          ",
+        );
+      });
+      test("whitespace between header indicators is illegal", () => {
+        assert.throws(
+          () => taglet`| 2
+            1
+            2
+            3
+          `,
+          (e) => e instanceof taglet.error.BlockHeaderError,
+        );
+        assert.throws(
+          () => taglet`| -
+            1
+            2
+            3
+          `,
+          (e) => e instanceof taglet.error.BlockHeaderError,
+        );
+        assert.throws(
+          () => taglet`|2 +
+            1
+            2
+            3
+          `,
+          (e) => e instanceof taglet.error.BlockHeaderError,
+        );
+        assert.throws(
+          () => taglet`|- 2
+            1
+            2
+            3
+          `,
+          (e) => e instanceof taglet.error.BlockHeaderError,
+        );
+        assert.throws(
+          () => taglet`| + 2
+            1
+            2
+            3
+          `,
+          (e) => e instanceof taglet.error.BlockHeaderError,
+        );
+        assert.throws(
+          () => taglet`| 2 -
+            1
+            2
+            3
+          `,
+          (e) => e instanceof taglet.error.BlockHeaderError,
+        );
+      });
+      test("whitespace after header is illegal", () => {
+        assert.throws(
+          () => taglet`| 
+            1
+            2
+            3
+          `,
+          (e) => e instanceof taglet.error.BlockHeaderError,
+        );
+      });
+    });
+    describe("indention inference", () => {
+      test("empty leading lines are ignored", () => {
+        assert.equal(
+          taglet`|
+
+  
+    
+      
+        
+          
+            1
+              2
+                3
+          `.toString(),
+          "\n\n\n\n\n\n1\n  2\n    3\n",
+        );
+      });
+      test("non-empty blank leading lines are illegal", () => {
+        assert.throws(
+          () => taglet`|
+              
+            1
+              2
+                3
+          `,
+          {
+            message:
+              "[line 1] WhiteSpaceError: too much whitespace on line prior to first non-empty line",
+          },
+        );
+      });
+      test("all non-blank lines following the first non-blank line must share its indention", () => {
+        assert.throws(
+          () => taglet`|
+              
+                1
+              2
+            3
+          `,
+          {
+            message: "[line 3] WhiteSpaceError: insufficient indentation",
+          },
+        );
+      });
+    });
+    describe("literal block templates", () => {
+      describe("indention", () => {
+        test("strips indention", () => {
+          assert.equal(
+            taglet`|
+              1
+              2
+              3
+            `.toString(),
+            "1\n2\n3\n",
+          );
+        });
+        test("preserves semantic indention", () => {
+          assert.equal(
+            taglet`|
+              1
+                2
+              3
+            `.toString(),
+            "1\n  2\n3\n",
+          );
+        });
+        test("allows indention with tabs", () => {
+          assert.equal(
+            taglet`|
+              1
+                2
+              3
+        `.toString(),
+            "1\n  2\n3\n",
+          );
+        });
+        test("allows indention with tabs and spaces", () => {
+          assert.equal(
+            taglet`|
+              1
+                2
+              3
+        `.toString(),
+            "1\n  2\n3\n",
+          );
+        });
+        test("preserves indention for arguments with new lines", () => {
+          assert.equal(
+            taglet`|
+              1
+                ${"2\n3"}
+              4
+            `.toString(),
+            "1\n  2\n  3\n4\n",
+          );
+        });
+        test("semantic whitespace before the first non-empty line is illegal", () => {
+          assert.throws(() =>
+            taglet`|
+                
+              1
+              2
+            `.toString(),
+          );
+        });
+      });
+    });
+    describe("folded block templates", () => {
+      test("folds adjacent non-blank lines", () => {
+        assert.equal(
+          taglet`>
+          1
+          2
+          3
+        `.toString(),
+          "1 2 3\n",
+        );
+      });
+      test("doesn't fold blank lines", () => {
+        assert.equal(
+          taglet`>
+          1
+
+          2
+          3
+        `.toString(),
+          "1\n\n2 3\n",
+        );
+      });
+      test("doesn't fold lines with semantic indention", () => {
+        assert.equal(
+          taglet`>
+          1
+            2
+        `.toString(),
+          "1\n  2\n",
+        );
+        assert.equal(
+          taglet`>8
+          1
+          2
+        `.toString(),
+          "  1\n  2\n",
+        );
+      });
+      test("an escaped line ending prevents folding", () => {
+        assert.equal(
+          taglet`>
+          line one\
+          line two
+        `.toString(),
+          "line one\nline two\n",
+        );
+      });
+    });
+    describe("chomping: clip", () => {
+      test("leaves one trailing new line", () => {
+        assert.equal(
+          taglet`|
+            1`.toString(),
+          "1\n",
+        );
+        assert.equal(
+          taglet`|
+            1
+          `.toString(),
+          "1\n",
+        );
+        assert.equal(
+          taglet`|
+            1
+    
+          `.toString(),
+          "1\n",
+        );
+        assert.equal(
+          taglet`|
+            1
+    
+    
+    
+    
+    
+    
+          `.toString(),
+          "1\n",
+        );
+      });
+      test("preserves whitespace beyond the trimmed indention", () => {
+        assert.equal(
+          taglet`|
+            1
+              
+          `.toString(),
+          "1\n  \n",
+        );
+      });
+      test("treats non-empty blank line as any other non-empty line", () => {
+        assert.equal(
+          taglet`|
+            1
+            2
+            
+            
+              
+          `.toString(),
+          "1\n2\n\n\n  \n",
+        );
+      });
+      test("an escaped line ending folds two lines without a space", () => {
+        assert.equal(
+          taglet`|
+          line one\
+          -thousand
+        `.toString(),
+          "line one-thousand\n",
+        );
+      });
+      test("semantic whitespace before first non-blank line is illegal", () => {
+        assert.throws(
+          () => taglet`|
+          
+        1
+        2
+        3
+      `,
+        );
+      });
+    });
+    describe("chomping: strip", () => {
+      test("trims all trailing new lines", () => {
+        assert.equal(
+          taglet`|-
+          1`.toString(),
+          "1",
+        );
+        assert.equal(
+          taglet`|-
+          1
+        `.toString(),
+          "1",
+        );
+        assert.equal(
+          taglet`|-
+          1
+    
+        `.toString(),
+          "1",
+        );
+        assert.equal(
+          taglet`|-
+          1
+    
+    
+    
+    
+    
+    
+        `.toString(),
+          "1",
+        );
+      });
+    });
+    describe("chomping: keep", () => {
+      test("leaves trailing new lines intact", () => {
+        assert.equal(
+          taglet`|+
+          1`.toString(),
+          "1",
+        );
+        assert.equal(
+          taglet`|+
+          1
+        `.toString(),
+          "1\n",
+        );
+        assert.equal(
+          taglet`|+
+          1
+    
+        `.toString(),
+          "1\n\n",
+        );
+        assert.equal(
+          taglet`|+
+          1
+    
+    
+    
+    
+    
+    
+        `.toString(),
+          "1\n\n\n\n\n\n\n",
+        );
+      });
+      test("treats non-empty blank line as any other non-empty line", () => {
+        assert.equal(
+          taglet`|-
+          1
+          2
+          
+          
+            
+        `.toString(),
+          "1\n2\n\n\n  ",
+        );
+      });
+    });
+  });
+  describe("escaping", () => {
+    test("backslashes normally do not need escaping", () => {
+      assert.equal(taglet`a\b`.toString(), "a\\b");
+    });
+    test("line endings can be escaped", () => {
+      assert.equal(
+        taglet`|-
+          1\
+          2
+        `.toString(),
+        "12",
+      );
+      assert.equal(
+        taglet`>-
+          1\
+          2
+        `.toString(),
+        "1\n2",
+      );
+    });
+    test("backslashes preceding end of line or escaped end of line must be escaped", () => {
+      assert.equal(
+        taglet`|-
+          1\\
+          2
+        `.toString(),
+        "1\\\n2",
+      );
+      assert.equal(
+        taglet`>-
+          1\\
+          2
+        `.toString(),
+        "1\\ 2",
+      );
+      assert.equal(
+        taglet`|-
+          1\\\
+          2
+        `.toString(),
+        "1\\2",
+      );
+      assert.equal(
+        taglet`>-
+          1\\\
+          2
+        `.toString(),
+        "1\\\n2",
+      );
+      assert.equal(
+        taglet`|-
+          1\\\\
+          2
+        `.toString(),
+        "1\\\\\n2",
+      );
+      assert.equal(
+        taglet`>-
+          1\\\\
+          2
+        `.toString(),
+        "1\\\\ 2",
+      );
+    });
+  });
+});
